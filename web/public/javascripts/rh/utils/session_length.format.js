8a07a0986e53219c94c0ae0a5f23c81de7563d6d
mkrecny <mkrecny@gmail.com>
2012-07-17 11:28:15 -0400

    session lengths are now 24hrly

--- web/public/javascripts/rh/utils/session_length.format.js
+++ web/public/javascripts/rh/utils/session_length.format.js
@@ -5,7 +5,7 @@
    * take the avg
    */
   var shift = utils.time_shift;
-  var floor_midnight = utils.floor_midnight;
+  var floor_day = utils.floor_midnight;
 
   function get_map_numerical_avg(map){
     if (!map) return 0;
@@ -22,22 +22,34 @@
     return ms/1000/60;
   };
 
-  function condense(data, reduce_key){
-    var condensed = {};
-    Object.keys(data).forEach(function(key){
-      var _key = reduce_key(key);
+  function array_avg(a){
+    var sum = 0;
+    a.forEach(function(val){
+      sum+=(val/1);
     });
-
+    return sum/a.length;
   };
+
 
   window.utils.session_length_format = function(data){
     var out = [];
+    var map = {};
+    
+    // map of tsdays to array of s lens
+    Object.keys(data).forEach(function(tshr){
+      var tsday = floor_day(tshr);
+      map[tsday] = map.hasOwnProperty(tsday) ? map[tsday].concat(_.values(data[tshr])) : _.values(data[tshr]);
+    });
 
-    //data = condense(data, floor_midnight);
+    // rewrite arrays as avg val of arrays
+    Object.keys(map).forEach(function(tsday){
+      map[tsday] = array_avg(map[tsday]);
+    });
 
-    Object.keys(data).forEach(function(ts){
+    Object.keys(map).forEach(function(ts){
       //out.push([shift(ts/1), ms_to_mins(get_map_numerical_avg(data[ts]))]);
-      out.push([ts/1, ms_to_mins(get_map_numerical_avg(data[ts]))]);
+      //out.push([ts/1, ms_to_mins(get_map_numerical_avg(data[ts]))]);
+      out.push([ts/1, ms_to_mins(map[ts])]);
     });
 
     return out;
